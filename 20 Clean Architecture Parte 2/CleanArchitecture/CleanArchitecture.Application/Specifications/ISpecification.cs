c9b22ad539b92920e8efa845bd43f9263d7cd496
JoshDiaz <josethp1@hotmail.com>
2026-09-21 22:00:25 -0500

    Agregando Patron Specificacion CleanArchitecture

---
+++ 20 Clean Architecture Parte 2/CleanArchitecture/CleanArchitecture.Application/Specifications/ISpecification.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+namespace CleanArchitecture.Application.Specifications
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
